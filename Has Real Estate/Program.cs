71ea6150b4f0beaf5e27a2507ecf3ff5bbd722fb
Yara M Esmaiel <yaraesmaiel123@gmail.com>
2024-07-02 17:25:04 +0300

    services

--- Has Real Estate/Program.cs
+++ Has Real Estate/Program.cs
@@ -1,4 +1,5 @@
 using Has_Real_Estate.Data;
+using Has_Real_Estate.Services.IServices;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,11 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IHomeRepo, HomeRepo>();
+builder.Services.AddScoped<IImageService, ImageService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
