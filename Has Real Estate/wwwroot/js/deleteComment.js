f03adbd495df8132aa26c46b5edd1e0a55a39e52
Yara M Esmaiel <yaraesmaiel123@gmail.com>
2024-07-11 19:49:05 +0300

    s

--- Has Real Estate/wwwroot/js/deleteComment.js
+++ Has Real Estate/wwwroot/js/deleteComment.js
@@ -1,47 +1,51 @@
-﻿ 
-    $(document).ready(function () {
-        $('.js-delete').on('click', function () {
-            var btn = $(this);
+﻿$(document).ready(function () {
+    var del = $('.yy');
+    $('.js-delete').on('click', function () {
+        var btn = $(this);
 
-            const swal = Swal.mixin({
-                customClass: {
-                    confirmButton: 'btn btn-danger mx-2',
-                    cancelButton: 'btn btn-light'
-                },
-                buttonsStyling: false
-            });
+        const swal = Swal.mixin({
+            customClass: {
+                confirmButton: 'btn btn-danger mx-2',
+                cancelButton: 'btn btn-light'
+            },
+            buttonsStyling: false
+        });
 
-            swal.fire({
-                title: 'Are you sure that you need to delete this Comment?',
-                text: "You won't be able to revert this!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonText: 'Yes, delete it!',
-                cancelButtonText: 'No, cancel!',
-                reverseButtons: true
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    $.ajax({
-                        url: `/User/RemoveComment/${btn.data('id')}`,
-                        method: 'DELETE',
-                        success: function () {
-                            swal.fire(
-                                'Deleted!',
-                                'Comment has been deleted.',
-                                'success'
-                            );
-                            $("div#currentDiv").remove();
-                        },
-                        error: function () {
-                            swal.fire(
-                                'Oooops...',
-                                'Something went wrong.',
-                                'error'
-                            );
-                        }
-                    });
-                }
-            });
+        swal.fire({
+            title: 'Are you sure that you need to delete this Comment?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: `/User/RemoveComment/${btn.data('id')}`,
+                    method: 'DELETE',
+                    success: function () {
+                        swal.fire(
+                            'Deleted!',
+                            'Comment has been deleted.',
+                            'success'
+                        );
+                        // // $("#div1").fadeOut();
+                        // $(`#comment-${commentId}`).remove();
+                        // btn.parents('div').fadeOut();
+                        del.fadeOut();
+                        // $(this).parents('div').closest('.yy').fadeOut();
+                        console.log("Hello world!");
+                    },
+                    error: function () {
+                        swal.fire(
+                            'Oooops...',
+                            'Something went wrong.',
+                            'error'
+                        );
+                    }
+                });
+            }
         });
-        });
- 
+    });
+});
---
+++ Has Real Estate/wwwroot/js/mapdetail.js
@@ -0,0 +1,39 @@
+﻿function GetMap() {
+
+
+    ///////////////Converting the C# Model to JS model
+    // convsert the data base table to Array in json type
+    var home = @Html.Raw(Json.Serialize(Model));
+
+
+    //You can customize the map as you load it.
+    // Use the following code to update how the map is loaded in the GetMap function such that it sets the type to aerial,
+    // the zoom level to 10, and the map center over London, UK (51.50632, -0.12714).
+    var map = new Microsoft.Maps.Map('#myMap', {
+        credentials: 'ArTYnXAo5ajG1e4ZyFIV-LT3sW_IU0Uy2YETN7N-u5EdkJOaOwaft6BOA1SPlVfA',
+        center: new Microsoft.Maps.Location(home.longitude, home.latitude),
+        mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+        zoom: 10
+    });
+
+    var location = new Microsoft.Maps.Location(home.longitude, home.latitude);
+    var pin = new Microsoft.Maps.Pushpin(location, {
+        color: 'green',
+        title: home.name,
+    });
+    map.entities.push(pin);
+
+    //////////////////Pushpins, sometimes known as MapIcons or Markers,
+    // are the primary method in the Bing Maps V8 Map Control to add a graphical image and text at a location within the map.
+    //Create custom Pushpin
+    // هنا يمكننا وضع الاحداثيات بدل سينتر لتثبيت ماركر
+
+    var myloc = new Microsoft.Maps.Location(35.340192118895025, 35.93475407561252);
+    var pin = new Microsoft.Maps.Pushpin(myloc, {
+        color: 'red',
+        title: 'Jableh',
+        subTitle: 'City Center',
+
+    });
+    map.entities.push(pin);
+} // end of Getmap()
